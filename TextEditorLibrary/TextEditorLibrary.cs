57ff65b28f2e7acb9e06b2941488a244fe1982a7
blackjacksike <46088869+blackjacksike@users.noreply.github.com>
2021-04-23 18:12:45 -0400

    Lots of changes
    - Declared all private members into a single line
    - Created the method 'IsRightType()' to make sure the user can only edit or create .txt files
    - Rendered the creation of a file via the 'EditFile()' method impossible
    - Put most code blocks within try...catch statements to intercept any exception.

--- TextEditorLibrary/TextEditorLibrary.cs
+++ TextEditorLibrary/TextEditorLibrary.cs
@@ -5,42 +5,104 @@ namespace TextEditorLibrary
 {
     public class TextFile
     {
-        private string fileName;
-        private string folderPath;
-        private string filePath;
-        private string content;
+        private string fileName, folderPath, filePath, content;
 
+        public bool IsRightType()
+        {
+            if (Path.GetExtension(filePath) == ".txt")
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        
         public void CreateFile()
         {
-            Console.WriteLine("Where do you want to create your text file?");
-            folderPath = Console.ReadLine();
-            Console.WriteLine("Please name your text file.");
-            fileName = Console.ReadLine();
-            filePath = (folderPath.EndsWith('\\') == true) ? String.Concat(folderPath, fileName) : String.Concat(folderPath + "\\", fileName);
-            /* If the path ends with '\', then it will concatenate. Otherwise, it will add that character and then concatenate. */
-            File.Create(filePath);
+            /* First problem: You could create any type of file, not only *.txt. [Solved] */
+
+            try
+            {
+                Console.WriteLine("Where do you want to create your text file?");
+                folderPath = Console.ReadLine();
+                Console.WriteLine("Please name your text file.");
+                fileName = Console.ReadLine();
+                filePath = (folderPath.EndsWith('\\') == true) ? String.Concat(folderPath, fileName) : String.Concat(folderPath + "\\", fileName);
+                /* If the path ends with '\', then it will concatenate. Otherwise, it will add that character and then concatenate. */
+                if (IsRightType() == true) // can only create the right type of file
+                {
+                    File.Create(filePath);
+                }
+                else 
+                {
+                    Console.WriteLine("Wrong extension.");
+                }
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         public void EditFile()
         {
-            Console.WriteLine("Where is the file you want to edit?");
-            folderPath = Console.ReadLine();
-            Console.WriteLine("Which file do you want to edit?");
-            fileName = Console.ReadLine();
-            filePath = (folderPath.EndsWith('\\') == true) ? String.Concat(folderPath, fileName) : String.Concat(folderPath + "\\", fileName);
-            Console.WriteLine("Please enter whatever you like.");
-            content = Console.ReadLine();
-            File.WriteAllText(filePath, content);
+            /* First problem: Editing a file can also mean creating a file [Solved]. Second problem: You can only edit one line of text.
+            Third problem: You can't list all files within a folder. */
+            
+            try
+            {
+                Console.WriteLine("Where is the file you want to edit?");
+                folderPath = Console.ReadLine();
+                Console.WriteLine("Which file do you want to edit?");
+                fileName = Console.ReadLine();
+                filePath = (folderPath.EndsWith('\\') == true) ? String.Concat(folderPath, fileName) : String.Concat(folderPath + "\\", fileName);
+
+                if (File.Exists(filePath) == true && IsRightType() == true) // if the file exists and is the right type, then you can edit it.
+                {
+                    Console.WriteLine("Please enter whatever you like.");
+                    content = Console.ReadLine();
+                    File.WriteAllText(filePath, content);
+                }
+                else if (File.Exists(filePath) == true) // if the file exists, that means that it simply wasn't the right type.
+                {
+                    Console.WriteLine("Wrong extension.");
+                }
+                else // failure to meet the above requirement means that both conditions were false.
+                {
+                    Console.WriteLine($"The file {fileName} does not exist in the path {folderPath}.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         public void DeleteFile()
         {
-            Console.WriteLine("Where is the file you want to delete?");
-            folderPath = Console.ReadLine();
-            Console.WriteLine("Which file do you want to delete?");
-            fileName = Console.ReadLine();
-            filePath = (folderPath.EndsWith('\\') == true) ? String.Concat(folderPath, fileName) : String.Concat(folderPath + "\\", fileName);
-            File.Delete(filePath);
+            try
+            {
+                Console.WriteLine("Where is the file you want to delete?");
+                folderPath = Console.ReadLine();
+                Console.WriteLine("Which file do you want to delete?");
+                fileName = Console.ReadLine();
+                filePath = (folderPath.EndsWith('\\') == true) ? String.Concat(folderPath, fileName) : String.Concat(folderPath + "\\", fileName);
+
+                if (File.Exists(filePath) == true) // can't delete a file that doesn't exist AND the user can delete any type of file for practical reasons
+                {
+                    File.Delete(filePath);
+                }
+                else
+                {
+                    Console.WriteLine($"The file {fileName} does not exist in the path {folderPath}.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
